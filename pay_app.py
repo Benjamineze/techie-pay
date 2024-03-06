79377f21888436be7d580b2a71e717521480ec69
Benjamin Eze <62108933+Benjamineze@users.noreply.github.com>
2024-03-06 17:31:53 +0000

    Update pay_app.py

--- pay_app.py
+++ pay_app.py
@@ -1,6 +1,22 @@
 import streamlit as st
 import sklearn
+import toml
 
+# Load custom theme configuration
+with open("custom_theme.toml", "r") as theme_file:
+    custom_theme = toml.load(theme_file)
+
+# Extract the 'theme' dictionary from the loaded configuration
+theme_settings = custom_theme.get("theme", {})
+
+# Apply the custom theme using **kwargs
+st.set_page_config(
+    page_title="techie-pay",
+    page_icon="Business-Technology-Digital-High-Tech-World-Background.png",
+    layout="centered",
+    initial_sidebar_state="collapsed",
+    **theme_settings
+)
 from predict_pg import show_predict_pg
 
 
